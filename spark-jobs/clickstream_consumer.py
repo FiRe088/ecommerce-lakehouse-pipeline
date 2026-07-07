b9a32fc3ff6786cbb8b9cac000f0b392acf78206
FiRe088 <chandan.suthar.ie@gmail.com>
2026-07-08 04:11:49 +0530

    feat: add timestamp conversion and windowed aggregation with watermarking

--- spark-jobs/clickstream_consumer.py
+++ spark-jobs/clickstream_consumer.py
@@ -4,7 +4,7 @@ os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
+from pyspark.sql.functions import from_json, col, to_timestamp, window
 from pyspark.sql.types import StructType, StructField, StringType
 
 spark = SparkSession.builder \
@@ -34,12 +34,28 @@ raw_df = spark.readStream \
 
 parsed_df = raw_df.selectExpr("CAST(value AS STRING) as json_value") \
     .select(from_json(col("json_value"), clickstream_schema).alias("data")) \
-    .select("data.*")
+    .select("data.*") \
+    .withColumn("event_time", to_timestamp(col("timestamp")))
 
 query = parsed_df.writeStream \
     .format("console") \
     .outputMode("append") \
     .option("checkpointLocation", "C:/spark-checkpoints/clickstream_consumer") \
+    .start()
+
+windowed_counts = parsed_df \
+    .withWatermark("event_time", "1 minute") \
+    .groupBy(
+        window(col("event_time"), "1 minute"),
+        col("event_type")
+    ) \
+    .count()
+
+query = windowed_counts.writeStream \
+    .format("console") \
+    .outputMode("update") \
+    .option("truncate", "false") \
+    .option("checkpointLocation", "C:/spark-checkpoints/clickstream_windowed") \
     .start()
 
 query.awaitTermination()
